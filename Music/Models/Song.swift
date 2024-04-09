42537b10f8eea3464b58cd63c141c938af7896b3
nguyenvanduyb <nguyen.van.duy-b@sun-asterisk.com>
2024-04-09 10:52:20 +0700

    add detail song screen

--- Music/Models/Song.swift
+++ Music/Models/Song.swift
@@ -15,12 +15,13 @@ struct Song {
     
     static func getPlaylist() -> [Song] {
         var songs: [Song] = []
+        songs.append(Song(name: "Yêu lại từ đầu", fileName: "YeuLaiTuDau", performer: "Khắc Việt", thumbnail: "yeulaitudaupicture"))
         songs.append(Song(name: "Em luôn ở trong tâm trí anh", fileName: "EmLuonOTrongTamTriAnh", performer: "The Men", thumbnail: "emluonotrongtamtrianhpicture"))
         songs.append(Song(name: "Hãy để anh yêu em lần nữa", fileName: "HayDeAnhYeuEmLanNua", performer: "The Men", thumbnail: "haydeanhyeuemlannuapicture"))
         songs.append(Song(name: "Lời anh muốn nói", fileName: "LoiAnhMuonNoi", performer: "The Men", thumbnail: "loianhmuonnoipicture"))
         songs.append(Song(name: "Ngọt ngào và đắng cay", fileName: "NgotNgaoVaDangCay", performer: "The Men", thumbnail: "ngotngaovadangcaypicture"))
-        songs.append(Song(name: "Người ấy", fileName: "Nguoiay", performer: "Trịnh Thăng Bình", thumbnail: "nguoiaypicture"))
-        songs.append(Song(name: "Yêu lại từ đầu", fileName: "EmLuonOTrongTamTriAnh", performer: "Khắc Việt", thumbnail: "yeulaitudaupicture"))
+        songs.append(Song(name: "Người ấy", fileName: "NguoiAy", performer: "Trịnh Thăng Bình", thumbnail: "nguoiaypicture"))
+
         return songs
     }
 }
